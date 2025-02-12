dafe923ea8835854290180ea8a954c0d604f7bdc
Asia-parveen <157685159+Asia-parveen@users.noreply.github.com>
2025-02-12 04:23:11 -0800

    added

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -11,9 +11,9 @@ const Nav = ({ openNav }:Props) => {
   return (
     <div className="h-[13vh] bg-[#181a1d]">
       <div className="flex items-center justify-between w-[90%] xl:w-[80%] h-full mx-auto">
-        <div className="text-[23px] md:text-[33px]  font-semibold text-white">
-          Brit<span className="text-[#E487FF]">sols </span>Comp
-          <span className="text-[#E487FF]">any</span>
+        <div className="text-[20px] sm:text-[25px] md:text-[27px] lg:text-[30px]  font-semibold text-white">
+          Brit<span className="text-[#E487FF]">sols</span>.
+          <span className="text-[#E487FF]">com</span>
         </div>
         <ul className="lg:flex hidden text-white items-center space-x-6 xl:space-x-10">
           <li className="navlink">
@@ -38,13 +38,13 @@ const Nav = ({ openNav }:Props) => {
         {/* download manu */}
         <div className="flex items-center space-x-6">
           <Link href="#">
-            <button className="relative h-12 w-48 overflow-hidden rounded-full bg-[#E487FF] text-lg shadow group">
+            <button className="relative h-8 sm:h-10 w-[100px] sm:w-[120px] overflow-hidden rounded-full bg-[#E487FF] text-lg shadow group">
               {/* Expanding white background on hover */}
               <span className="absolute inset-0 w-3 bg-white transition-all duration-300 ease-out group-hover:w-full"></span>
 
               {/* Text color change effect */}
               <span className="relative flex items-center justify-center space-x-3 text-white font-semibold group-hover:text-[#AF3cFF] transition-colors duration-300">
-                <span>Sign In</span>
+                <span className="text-[14px]">Sign In</span>
               </span>
             </button>
           </Link>
