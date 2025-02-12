e2f871187b96d18ede36bc899b6646c80f948793
Asia-parveen <157685159+Asia-parveen@users.noreply.github.com>
2025-02-12 05:31:21 -0800

    added

--- src/app/components/First.tsx
+++ src/app/components/First.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const First = () => {
   return (
-    <div className='max-w-screen-2xl mx-auto flex justify-center item-center pt-[35%] sm:pt-[28%] md:pt-[18%] '>
-      <h1 className='  text-[22px] px-[10px] sm:text-[38px] font-bold md:text-[35px] lg:text-[36px] xl:text-[40px] '><span className='text-white'>Join  </span> <span className='text-[#E487FF]'>Britosols</span> <span className='text-white'> Company</span></h1>
+    <div className='max-w-screen-2xl mx-auto flex justify-center item-center pt-[35%] sm:pt-[28%] md:pt-[18%] lg:pt-[14%] '>
+      <h1 className='  text-[22px] px-[10px] sm:text-[38px] font-bold md:text-[35px] lg:text-[33px] xl:text-[38px] '><span className='text-white'>Join  </span> <span className='text-[#E487FF]'>Britosols</span> <span className='text-white'> Company</span></h1>
     </div>
   )
 }
